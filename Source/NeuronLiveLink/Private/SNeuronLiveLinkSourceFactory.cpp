6d3edc737bd98cf4edb5af0949d4ea32a945b4d6
lijinlong <gmlijinlong@gmail.com>
2022-07-12 12:19:58 +0800

    Commmand communication UI

--- Source/NeuronLiveLink/Private/SNeuronLiveLinkSourceFactory.cpp
+++ Source/NeuronLiveLink/Private/SNeuronLiveLinkSourceFactory.cpp
@@ -105,7 +105,7 @@ void SNeuronLiveLinkSourceFactory::Construct( const FArguments& Args )
 	ChildSlot
 		[
 			SNew( SBox )
-				.WidthOverride( 250 )
+				.WidthOverride( 300 )
 				[
 					SNew( SVerticalBox )
 					+ SVerticalBox::Slot( )
